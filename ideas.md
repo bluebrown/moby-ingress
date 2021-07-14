2c145fcdb42fa47573ba76a4d1385b05af01dca4
Nico Braun <nico-braun@live.de>
2021-07-14 11:35:54 +0000

    only reload on config change

---
+++ ideas.md
@@ -0,0 +1,4 @@
+# Ideas
+
+- accept config template as post request to manager
+- only restart haproxy if config has changed
--- loadbalancer/entrypoint.sh
+++ loadbalancer/entrypoint.sh
@@ -1,10 +1,36 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-./background.sh &
+fetch_config() 
+{
+    curl -s -f "$1" > haproxy.cfg
+    md5sum haproxy.cfg
+}
 
-curl -s -f "$MANAGER_ENDPOINT" > haproxy.cfg  
-# compare cache vs new
+scrape_config()
+{
+    while :
+        do
+            sleep 60
+            old_sum="$(md5sum haproxy.cfg)"
+            sum="$(fetch_config "$1")"
+            if [ "$old_sum" = "$sum" ]
+            then
+                echo "config has not changed"
+            else
+                echo "config has changed, reloading worker"
+                kill -s SIGUSR2 1
+            fi
+        done
+}
+
+# run task in background to update config and restart proxy
+scrape_config "$MANAGER_ENDPOINT" &
+
+# get inital config
+fetch_config "$MANAGER_ENDPOINT"
+
+# exec original entrypoint to make it pid 1
 exec docker-entrypoint.sh haproxy -f /usr/local/etc/haproxy/haproxy.cfg
 
