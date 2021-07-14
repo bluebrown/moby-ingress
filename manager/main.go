97a96bd08030c4b6ab8b24fe13772c1230924ae8
Nico Braun <nico-braun@live.de>
2021-07-14 02:09:48 +0000

    extract all config sections

--- manager/main.go
+++ manager/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
 )
+
+type templateData struct {
+	ManagerInfo types.ContainerJSON
+	Services    []swarm.Service
+}
 
 func main() {
 
@@ -29,14 +37,24 @@ func main() {
 	t := template.Must(template.New(name).Funcs(sprig.TxtFuncMap()).ParseFiles(*templatPath))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
 		services, err := cli.ServiceList(ctx, types.ServiceListOptions{})
 		if err != nil {
-			panic(err)
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 
+		info, err := cli.ContainerInspect(ctx, os.Getenv("HOSTNAME"))
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		fmt.Println(info.Config.Labels)
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
-		t.Execute(w, services)
+		t.Execute(w, templateData{info, services})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
