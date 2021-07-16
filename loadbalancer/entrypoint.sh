981060e2fe9795acb9ec01af3f5676a30b88fab4
Nico Braun <nico-braun@live.de>
2021-07-16 15:34:16 +0000

    Revert "fork background task to scrape config via haproxy"
    This reverts commit d2fbb7c0282ad268c2f29dccec7f4913375020b7.

--- loadbalancer/entrypoint.sh
+++ loadbalancer/entrypoint.sh
@@ -2,6 +2,46 @@
 
 set -e
 
+scrape_config()
+{
+    # take the inital checksum
+    sum="$(md5sum haproxy.cfg)"
+
+    while :
+        do
+            # sleep until next iteration
+            sleep "$2"
+
+            # try to fetch the config
+            if ! curl -s -f "$1" > haproxy.cfg;
+            then
+                cp previous.cfg haproxy.cfg
+                continue
+            fi
+
+            # compare check sums
+            old_sum="$sum"
+            sum="$(md5sum haproxy.cfg)"
+            if [ "$old_sum" = "$sum" ]
+            then 
+                continue
+            fi
+
+            # test if file is valid
+            if ! haproxy -c -f haproxy.cfg;
+            then
+                cp previous.cfg haproxy.cfg
+                sum="$old_sum"
+                continue
+            fi
+
+            # if file has been successfully fetched, 
+            # has a different checksum and is valid,
+            # reload the worker
+            kill -s SIGUSR2 1
+        done
+}
+
 # make a backup
 cp haproxy.cfg previous.cfg
 
@@ -15,6 +55,9 @@ then
 else
     cp previous.cfg haproxy.cfg
 fi
+
+# run task in background  every minute to update config and restart if needed proxy
+scrape_config "$MANAGER_ENDPOINT" "${SCRAPE_INTERVAL:=60}" &
 
 # exec original entrypoint to make it pid 1
 exec docker-entrypoint.sh "$@"
