71266772631cfca3fe3247bb21ad76410c784ded
Nico Braun <nico-braun@live.de>
2021-07-17 20:03:26 +0000

    capture shtudown signals

--- hapi/main.go
+++ hapi/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"strconv"
 	"strings"
+	"syscall"
 	"text/template"
 	"time"
 
@@ -20,65 +24,101 @@ import (
 )
 
 var (
-	myClient     = &http.Client{Timeout: 10 * time.Second}
-	configPath   *string
-	templatePath *string
-	t            *template.Template
+	myClient        = &http.Client{Timeout: 10 * time.Second}
+	configPath      string
+	templatePath    string
+	managerEndpoint string
+	t               *template.Template
 )
 
 func main() {
-	configPath = flag.String("config", "/usr/local/etc/haproxy/haproxy.cfg", "path to config inside the container")
-	configPath = flag.String("c", "/usr/local/etc/haproxy/haproxy.cfg", "path to config inside the container")
-	templatePath = flag.String("template", "/usr/local/etc/haproxy/haproxy.templ.cfg", "path to template inside the container")
-	flag.Parse()
 
-	parts := strings.Split(*templatePath, "/")
-	name := parts[len(parts)-1]
-	t = template.Must(template.New(name).Funcs(sprig.TxtFuncMap()).ParseFiles(*templatePath))
+	configPath = getenv("CONFIG_PATH", "/usr/local/etc/haproxy/haproxy.cfg")
+	templatePath = getenv("TEMPLATE_PATH", "/usr/local/etc/haproxy/haproxy.templ.cfg")
+	managerEndpoint = getenv("MANAGER_ENDPOINT", "http://manager:8080/json")
 
-	for {
-		time.Sleep(time.Second * 30)
-		r, err := myClient.Get("http://manager:8080/json")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		// read the post body to a map
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			fmt.Println(err)
-			r.Body.Close()
-			continue
-		}
-
-		var objmap map[string]interface{}
-		err = json.Unmarshal(body, &objmap)
-		if err != nil {
-			fmt.Println(err)
-			r.Body.Close()
-			continue
-		}
-
-		r.Body.Close()
-
-		err = processConf(objmap, t, *configPath)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		// reload worker is all is ok
-		cmd := exec.Command("kill", "-s", "SIGUSR2", "1")
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		fmt.Println("new config created and worker reloaded")
+	si := getenv("SCRAPE_INTERVAL", "30")
+	scrapeInterval, err := strconv.Atoi(si)
+	if err != nil {
+		fmt.Println(err)
+		scrapeInterval = 30
 	}
 
+	parts := strings.Split(templatePath, "/")
+	name := parts[len(parts)-1]
+	t = template.Must(template.New(name).Funcs(sprig.TxtFuncMap()).ParseFiles(templatePath))
+
+	tickspeed := time.Second * time.Duration(scrapeInterval)
+	bg := context.Background()
+	ctx, cancel := context.WithCancel(bg)
+	defer cancel()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(tickspeed):
+				r, err := myClient.Get(managerEndpoint)
+				if err != nil {
+					fmt.Printf("could not fetch from %s: %v", managerEndpoint, err)
+					continue
+				}
+
+				if r.StatusCode > 300 {
+					fmt.Printf("could not fetch from %s: response status: %v", managerEndpoint, r.Status)
+
+					continue
+				}
+
+				// read the post body to a map
+				body, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					fmt.Println("could not read body:", err)
+					r.Body.Close()
+					continue
+				}
+
+				var objmap map[string]interface{}
+				err = json.Unmarshal(body, &objmap)
+				if err != nil {
+					fmt.Println("could not map data:", err)
+					r.Body.Close()
+					continue
+				}
+
+				r.Body.Close()
+
+				err = processConf(objmap, t, configPath)
+				if err != nil {
+					fmt.Println("stopped processing conf:", err)
+					continue
+				}
+
+				// reload worker is all is ok
+				cmd := exec.Command("kill", "-s", "SIGUSR2", "1")
+				err = cmd.Run()
+				if err != nil {
+					fmt.Println("could not reload worker", err)
+					continue
+				}
+
+				fmt.Println("new config created and worker reloaded")
+			}
+		}
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(
+		signalChan,
+		syscall.SIGHUP,  // kill -SIGHUP XXXX
+		syscall.SIGINT,  // kill -SIGINT XXXX or Ctrl+c
+		syscall.SIGQUIT, // kill -SIGQUIT XXXX
+	)
+
+	<-signalChan
+	log.Print("os.Interrupt - shutting down...\n")
 }
 
 func processConf(objmap map[string]interface{}, t *template.Template, configPath string) error {
@@ -140,47 +180,10 @@ func md5sum(filePath string) (string, error) {
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 
-func PosrHandler(w http.ResponseWriter, r *http.Request) {
-
-	// only accept post method
-	if r.Method != "POST" {
-		fmt.Fprintln(w, "only post method is allowed")
-		w.WriteHeader(http.StatusBadRequest)
-		return
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
 	}
-
-	// read the post body to a map
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintln(w, "could not read post body")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	var objmap map[string]interface{}
-	err = json.Unmarshal(body, &objmap)
-	if err != nil {
-		fmt.Fprintln(w, "could not parse post body")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	err = processConf(objmap, t, *configPath)
-	if err != nil {
-		fmt.Fprintf(w, "not reloading worker due to %v\n", err)
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	// reload worker is all is ok
-	cmd := exec.Command("kill", "-s", "SIGUSR2", "1")
-	err = cmd.Run()
-	if err != nil {
-		fmt.Fprintln(w, "could not reload worker")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintln(w, "new config created and worker reloaded")
-	w.WriteHeader(http.StatusCreated)
-
+	return value
 }
